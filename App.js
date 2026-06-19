5771f9508da07c7744c72bb915eef79879b0b9f9
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-06-19 00:24:07 -0930

    Building my first mobile app

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet} from "react-native";
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello Cedric 👋</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+      backgroundColor:"yellow",
+    },
+    text:{
+      color:"blue",
+      fontSize:50,
+      fontWeight:"bold",
+    },
+  });
