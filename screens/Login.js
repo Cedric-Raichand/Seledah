f5eef589ff8460d867817294653e733461d85fca
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-06-29 21:27:01 -0930

    update login page

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,54 @@
 import {View, Button, StyleSheet, Text,TextInput} from "react-native"
 import { useState } from "react"
 
-export default function Login(){
+export default function Login({navigation}){
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function loginUser(){
+    if (username==="Cedric" && password=="1234"){
+      navigation.navigate("Home");
+    }
+    else{
+      alert("wrong username or password")
+    }
+  }
   return(
-    <View>
-      <TextInput placeholder="Enter username"
-      onChangeText={setUsername}/>
-      <Text>{username}</Text>
+    <View style ={styles.container}>
+      <Text>
+        login page
+      </Text>
+
+      <TextInput placeholder="Username" onChangeText={setUsername} style={styles.input}/>
+      <TextInput placeholder="Password" onChangeText={setPassword} style={styles.output}/>
+
+      <Button 
+      title="login"
+      onPress={loginUser} style={styles.you}/>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  input:{
+    width:300,
+    height:50,
+    borderWidth:1,
+    borderRadius:10
+  },
+  output:{
+    width:300,
+    height:50,
+    borderWidth:1,
+    borderRadius:10,
+    marginTop:20
+  },
+  you:{
+    marginTop:20
+  }
+})
 
 
